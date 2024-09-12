5d1f6bb678e84a6960a502aac743a60ff20ef621
Chunkit Yip <chunkit.yip@peopleshores.com>
2024-09-12 11:15:07 -0400

    test: implement BET testing with getCustomerById and searchCustomersByName method in customer service impl

--- src/main/java/com/example/demo/repo/CustomerRepository.java
+++ src/main/java/com/example/demo/repo/CustomerRepository.java
@@ -11,19 +11,6 @@ import java.util.List;
  */
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer,Long> {
-    /**
-     @Query("select c from Customer where id like :id")
-     List<Customer> searchCustomer(@Param("id") Long id);
-
-     msz: my correction like is for strings, i think it should be = since id is long
-     I'm just testing this query with h2. it can be uncommented tomorrow
-
-     @Query("select c from Customer c where c.id = :id")
-     List<Customer> searchCustomer(@Param("id") Long id);
-     */
-
     List<Customer> findCustomerByName(String customerName);
-
-
 }
 
--- src/test/java/com/example/demo/service/impl/CustomerServiceImplTest.java
+++ src/test/java/com/example/demo/service/impl/CustomerServiceImplTest.java
@@ -1,24 +1,25 @@
 package com.example.demo.service.impl;
 
 import com.example.demo.dto.CustomerDTO;
-import com.example.demo.dto.RegisterCustomerDTO;
+import com.example.demo.entity.BankAccount;
 import com.example.demo.entity.Customer;
-import com.example.demo.exception.*;
+import com.example.demo.enums.AccountStatus;
+import com.example.demo.exception.CustomerNotFoundException;
 import com.example.demo.repo.CustomerRepository;
-import org.junit.jupiter.api.BeforeEach;
+import com.example.demo.service.CustomerService;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
+import java.math.BigDecimal;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 class CustomerServiceImplTest {
 
     @Mock
@@ -27,97 +28,103 @@ class CustomerServiceImplTest {
     @InjectMocks
     private CustomerServiceImpl customerService;
 
-    private Customer customer;
-    private CustomerDTO customerDTO;
 
-    @BeforeEach
-    void setUp() {
-        MockitoAnnotations.openMocks(this);
-        customer = new Customer(1L , "CK Yip", Collections.emptyList());
-        customerDTO = new CustomerDTO(1L, "CK Yip" , Collections.emptyList());
-    }
-
-    // Behavior
     @Test
-    void testCustomerById() throws CustomerNotFoundException {
+     public void getCustomerByIdTestWithEmptyAccount() throws CustomerNotFoundException {
+        // Mock
+
+        // Stub
+        Customer customer = new Customer(1L , "CK" , Collections.emptyList());
         when(customerRepository.findById(1L)).thenReturn(Optional.of(customer));
 
+        // Inject
+
+        // Act
         CustomerDTO result = customerService.getCustomerById(1L);
-        assertEquals(customerDTO.getName(), result.getName());
+
+        // Assert
+        assertEquals(1L , result.getId());
+        assertEquals("CK" , result.getName());
+        assertEquals(Collections.emptyList() , result.getAccounts());
+
         verify(customerRepository , times(1)).findById(1L);
     }
 
     @Test
-    void testCreateCustomer() {
-        // When
-        when(customerRepository.save(any(Customer.class))).thenReturn(customer);
-
-        // Then
-        CustomerDTO result = customerService.createCustomer(customer.getName());
-
-        // Result
-        assertEquals(customerDTO.getName() , result.getName());
-        verify(customerRepository, times(1)).save(any(Customer.class));
+    public void getCustomerByIdNotFoundException() {
+        when(customerRepository.findById(2L)).thenReturn(Optional.empty());
+        assertThrows(CustomerNotFoundException.class , () -> customerService.getCustomerById(2L));
     }
 
     @Test
-    public void testSearchCustomersByName() {
-        // When
-        String keyword = "ck";
-        when(customerRepository.findCustomerByName(keyword)).thenReturn(Arrays.asList(customer));
+    public void getCustomerByIdTestWithAccount() throws CustomerNotFoundException {
+        // Mock
 
-        // Then
-        List<CustomerDTO> result = customerService.searchCustomersByName(keyword);
+        // Stub
+        BankAccount account = new BankAccount(1L, 111L, null, "sortCode1", "Account1", null, null, null, null, null);
+        Customer customer = new Customer(1L , "CK", Arrays.asList(account));
+        when(customerRepository.findById(customer.getId())).thenReturn(Optional.of(customer));
 
-        // Result
-        assertEquals(1, result.size());
-        assertEquals("CK Yip", result.get(0).getName());
+        // Inject
+
+        // Act
+        CustomerDTO result = customerService.getCustomerById(customer.getId());
+
+        // Assert
+        assertEquals("CK" , result.getName());
+
+        // Verity
+        verify(customerRepository , times(1)).findById(1L);
     }
 
     @Test
-    public void testSearchCustomersWithUserNotFound() {
-        // When
-        String keyword = "Lawrence";
-        when(customerRepository.findCustomerByName(keyword)).thenReturn(Arrays.asList(customer));
+    void searchCustomersByNameTestWithEmptyAccount() {
+        // Mock
 
-        // Then
-        List<CustomerDTO> result = customerService.searchCustomersByName(keyword);
+        // Stub
+        Customer customer = new Customer(1L , "CK" , Collections.emptyList());
+        when(customerRepository.findCustomerByName(customer.getName())).thenReturn(Arrays.asList(customer));
 
-        // Result
-        assertNotEquals("Lawrence", result.get(0).getName());
-    }
+        // Inject
 
+        // Act
+        List<CustomerDTO> result = customerService.searchCustomersByName(customer.getName());
 
-    // Edge
-    @Test
-    void testGetCustomerByIdNotFound() {
-        // Then
-        when(customerRepository.findById(1L)).thenReturn(Optional.empty());
+        // Assert
+        assertEquals("CK" , result.get(0).getName());
 
-        // Result
-        assertThrows(CustomerNotFoundException.class , () -> customerService.getCustomerById(1L));
+        // Verity
+        verify(customerRepository , times(1)).findCustomerByName("CK");
     }
 
     @Test
+    void searchCustomersByNameTestWithMulitpleAccount() {
+        // Mock
 
-    void testSearchCustomersByNameWithEmptyResult() {
-        when(customerRepository.findById(1L)).thenReturn(Optional.empty());
-        assertThrows(CustomerNotFoundException.class, () -> {
-            CustomerDTO result = customerService.getCustomerById(1L);
-        });
+        // Stub
+        BankAccount account1 = new BankAccount(1L, 111L, null, "sortCode1", "Account1", null, null, null, null, null);
+        BankAccount account2 = new BankAccount(2L, 112L, null, "sortCode1", "Account1", null, null, null, null, null);
+        Customer customer = new Customer(1L, "CK", Arrays.asList(account1 , account2));
+        when(customerRepository.findCustomerByName(customer.getName())).thenReturn(Arrays.asList(customer));
+
+        // Inject
+
+        // Act
+        List<Customer> result = customerRepository.findCustomerByName(customer.getName());
+
+        // Assert
+        assertEquals(2 ,result.get(0).getBankAccounts().size());
+        assertEquals("CK" , result.get(0).getName());
+
+        // Verity
+        verify(customerRepository , times(1)).findCustomerByName("CK");
     }
 
     @Test
-    void deleteCustomer() throws CustomerNotFoundException {
-        when(customerRepository.findById(1L)).thenReturn(Optional.of(customer));
-        customerService.deleteCustomer(1L);
-        verify(customerRepository, times(1)).delete(customer);
+    void createCustomerTest() {
     }
 
     @Test
-    void deleteCustomerWithNoSuchCustomer() {
-        when(customerRepository.findById(1L)).thenReturn(Optional.empty());
-        assertThrows(CustomerNotFoundException.class, () -> customerService.deleteCustomer(1L));
-        verify(customerRepository, never()).delete(any());
+    void deleteCustomerTest() {
     }
 }
